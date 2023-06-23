2fa59ce047f1c6d6015907f17c9de7adbddba407
Marty_mcfly <46539390+Smrdeveloper@users.noreply.github.com>
2023-06-23 16:05:48 +0300

    lab v0.2.py fix
    Изменение неправильного имени функции

--- lab v0.2.py
+++ lab v0.2.py
@@ -1,10 +1,10 @@
 try:
-    input = int(input("Выбери номер задачи(1 или 2):"))
-    if input == 1:
+    inputval = int(input("Выбери номер задачи(1 или 2):"))
+    if inputval == 1:
         print("Задание 1")
-    elif input == 2:
+    elif inputval == 2:
         print("Задание 2")
-    else: 
+    else:
         print("Неверный ввод")
 except ValueError:
     print("Неверный ввод")
